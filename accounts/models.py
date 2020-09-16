418cdd4f5b73cddd27b9ece6a35a0914fc42484d
hyekyoungpark <lovehk67@gmail.com>
2020-09-16 10:52:32 +0900

    add urls.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    address_new = models.CharField(max_length=500, null=True, blank=True)
+    address_old = models.CharField(max_length=500, null=True, blank=True)
+    phone = models.CharField(max_length=200, null=True, blank=True)
