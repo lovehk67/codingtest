d4063549be857cf47e969e2520a52e9d0178dd00
hyekyoungpark <lovehk67@gmail.com>
2020-09-16 09:32:58 +0900

    rollback

--- codingtest/user/models.py
+++ codingtest/user/models.py
@@ -3,4 +3,5 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     address_new = models.CharField(max_length=500, null=True, blank=True)
+    address_old = models.CharField(max_length=500, null=True, blank=True)
     phone = models.CharField(max_length=200, null=True, blank=True)
