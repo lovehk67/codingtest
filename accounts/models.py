d2f763abfee1e22fc9a96653efcf43f42c924429
hyekyoungpark <lovehk67@gmail.com>
2020-09-16 20:22:16 +0900

    admin help_text

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 class User(AbstractUser):
-    address_input = models.CharField(max_length=500, null=True, blank=True)
-    address_new = models.CharField(max_length=500, null=True, blank=True)
-    address_old = models.CharField(max_length=500, null=True, blank=True)
-    phone = models.CharField(max_length=200, null=True, blank=True)
+    address_input = models.CharField(max_length=500, null=True, blank=True, help_text=_('입력받은 주소'))
+    address_new = models.CharField(max_length=500, null=True, blank=True, help_text=_('도로명주소'))
+    address_old = models.CharField(max_length=500, null=True, blank=True, help_text=_('지번주소'))
+    phone = models.CharField(max_length=200, null=True, blank=True, help_text=_('전화번호'))
